d488c9cf1522c3a5705de87223f2df7b26d9093b
JohnSV18 <69915500+JohnSV18@users.noreply.github.com>
2021-02-05 17:47:54 -0500

    added flask server and workout.html

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import jinja2
 import os
+import pymongo
 
 from pprint import PrettyPrinter
 from flask import Flask, request, render_template
+from flask_pymongo import PyMongo
 
 ################################################################################
 ## SETUP
@@ -10,13 +12,19 @@ from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
-my_loader = jinja2.ChoiceLoader([
-    app.jinja_loader,
-    jinja2.FileSystemLoader('data'),
-])
-app.jinja_loader = my_loader
+# host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/highly-fit') + "?retryWrites=false"
+# app.config["MONGO_URI"] = host
+# mongo = PyMongo(app)
 
-pp = PrettyPrinter(indent=4)
+
+# my_loader = jinja2.ChoiceLoader([
+#     app.jinja_loader,
+#     jinja2.FileSystemLoader('data'),
+# ])
+# app.jinja_loader = my_loader
+# mongo = PyMongo(app)
+
+# pp = PrettyPrinter(indent=4)
 
 ################################################################################
 ## ROUTES
@@ -24,6 +32,34 @@ pp = PrettyPrinter(indent=4)
 
 @app.route('/')
 def home():
+        welcome = 'welcome to the '
 
-    return render_template('index.html') 
 
+        context = {
+            'welcome': welcome
+        }
+        
+
+        return render_template('index.html', **context) 
+
+@app.route('/workout')
+def workout():
+        workout = 'abs'
+        time_length = 200
+        difficulty = 'beginner'
+        equipment = 'weights'
+        description = 'Do 3 reps'
+
+        context = {
+            'workout' : workout,
+            'time_length': time_length,
+            'difficulty': difficulty,
+            'equipment': equipment,
+            'description': description
+        }
+
+        return render_template('workout.html', **context) 
+       
+
+if __name__ == "__main__":
+    app.run(debug=True)
